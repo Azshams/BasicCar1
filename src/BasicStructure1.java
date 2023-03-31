c590c58a58badbd8346a6da24a448041a801d3f4
Azshams <azshams@gmail.com>
2023-03-31 00:41:59 -0400

    the basic structure of the car is ready

---
+++ src/BasicStructure1.java
@@ -0,0 +1,10 @@
+import java.lang.reflect.AnnotatedType;
+
+public class BasicStructure1 {
+    String model;
+    String power;
+    String color;
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BasicStructure1 bmw =new BasicStructure1();
+                bmw.color="black";
+                bmw.model="A3";
+                bmw.power="5000cc";
+                
+
+
+
+    }
+}
